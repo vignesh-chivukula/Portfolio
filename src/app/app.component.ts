349901590c43ea53f2db755e62f61831d2d6d3d8
vignesh-cv <vignesh.chivukula@stonybrook.edu>
2020-07-28 23:04:03 -0400

    Portfolio code - Version 1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {Title} from "@angular/platform-browser";
+
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {
+  constructor(private title:Title){
+    this.title.setTitle("Vignesh's Portfolio");
+  }
+}
